9c24b9b7aa01761c0bb55de2bbaa353b036fadee
Woozy37 <david.beugnet@yahoo.fr>
2016-05-23 00:10:20 +0200

    Implementation player + camera + map hauteur + detection de collisions (bug sur la collision)

--- projecty/src/engineTester/MainGameLoop.java
+++ projecty/src/engineTester/MainGameLoop.java
@@ -1,4 +1,4 @@
- package engineTester;
+package engineTester;
 
 import java.util.ArrayList;
 
@@ -8,6 +8,7 @@ import org.lwjgl.util.vector.Vector3f;
 import entities.Camera;
 import entities.Entity;
 import entities.Light;
+import entities.Player;
 import models.RawModel;
 import models.TexturedModel;
 import renderEngine.DisplayManager;
@@ -39,10 +40,12 @@ public class MainGameLoop {
 		
 		
 		RawModel model = OBJLoader.loadObjModel("stall", loader);
+		RawModel playerModel = OBJLoader.loadObjModel("person", loader);
 		
 		TexturedModel staticModel  = new TexturedModel(model, new ModelTexture(loader.loadTexture("stallTexture")));
 		TexturedModel grass = new TexturedModel(OBJLoader.loadObjModel("grassModel", loader),
 				new ModelTexture(loader.loadTexture("grassTexture")));
+		TexturedModel playerTModel = new TexturedModel(playerModel, new ModelTexture(loader.loadTexture("white")));
 		grass.getTexture().setHasTransparency(true);
 		grass.getTexture().setUseFakeLighting(true);
 		ModelTexture texture = staticModel.getTexture();
@@ -50,26 +53,28 @@ public class MainGameLoop {
 		texture.setReflectivity(1);
 		
 		ArrayList<Entity> allEntity = new ArrayList<Entity>();
+		Player player = new Player(playerTModel, new Vector3f(0,0,-50), 0, 0, 0, 1);
 		Entity entity = new Entity(staticModel, new Vector3f(0,0,-50),0,0,0,1);
 		Entity herbe = new Entity(grass, new Vector3f(0,0,-40), 0, 0, 0, 1);
 		allEntity.add(herbe);
 		allEntity.add(entity);
 		
 		
+		
 		Light light = new Light(new Vector3f(3000,2000,2000), new Vector3f(1,1,1));
 		
-		Terrain terrain = new Terrain(0,-1,loader,texturePack, blendMap);
-		Terrain terrain2 = new Terrain(-1,-1,loader,texturePack, blendMap);
+		Terrain terrain = new Terrain(0,-1,loader,texturePack, blendMap, "heightMap");
 		
-		Camera camera = new Camera();
+		Camera camera = new Camera(player);
 		
 		MasterRenderer renderer = new MasterRenderer();
 		
 		while(!Display.isCloseRequested()){
 			entity.increaseRotation(0, 1, 0);
 			camera.move();
+			player.move(terrain);
+			renderer.processEntity(player);
 			renderer.processTerrain(terrain);
-			renderer.processTerrain(terrain2);
 			for(Entity object:allEntity){
 				renderer.processEntity(object);
 			}
--- projecty/src/entities/Camera.java
+++ projecty/src/entities/Camera.java
@@ -3,36 +3,40 @@ package entities;
 
 
 import org.lwjgl.input.Keyboard;
+import org.lwjgl.input.Mouse;
 import org.lwjgl.util.vector.Vector3f;
 
 public class Camera {
 	
+	private float distanceFromPlayer = 50;
+	private float angleAroundPLayer = 0;
+	
+	private static float distanceMinPlayer = 20f;
+	private static float distanceMaxPlayer = 120f;
+	private static float pitchMin = 20;
+	private static float pitchMax = 80;
+	
 	private Vector3f position = new Vector3f(0,1,0);
-	private float pitch;
-	private float yaw;
+	private float pitch = 20;
+	private float yaw = 0;
 	private float roll;
+	
+	private Player player;
+	public Camera(Player player){
+		this.player = player;
+	}
 	
 	public Camera(){}
 	
 	public void move(){
-		if(Keyboard.isKeyDown(Keyboard.KEY_Z)){
-			position.z-=1f;
-		}
-		if(Keyboard.isKeyDown(Keyboard.KEY_D)){
-			position.x+=1f;
-		}
-		if(Keyboard.isKeyDown(Keyboard.KEY_Q)){
-			position.x-=1f;
-		}
-		if(Keyboard.isKeyDown(Keyboard.KEY_S)){
-			position.z+=1f;
-		}
-		if(Keyboard.isKeyDown(Keyboard.KEY_A)){
-			position.y-=1f;
-		}
-		if(Keyboard.isKeyDown(Keyboard.KEY_E)){
-			position.y+=1f;
-		}
+		calculateZoom();
+		calculatePitch();
+		calculateAngleAroundPlayer();
+		float horizontalDistance = calculateHorizontalDistance();
+		float verticalDistance = calculateVerticalDistance();
+		calculateCameraPosition(horizontalDistance, verticalDistance);
+		this.yaw = 180 - (player.getRotY() + angleAroundPLayer);
+		
 	}
 
 	public Vector3f getPosition() {
@@ -49,6 +53,58 @@ public class Camera {
 
 	public float getRoll() {
 		return roll;
+	}
+	
+	private void calculateCameraPosition(float horizDistance, float verticDistance){
+		float theta = player.getRotY() + angleAroundPLayer;
+		float offsetX = (float) (horizDistance * Math.sin(Math.toRadians(theta)));
+		float offsetZ = (float) (horizDistance * Math.cos(Math.toRadians(theta)));
+		position.x = player.getPosition().x - offsetX;
+		position.z = player.getPosition().z - offsetZ;
+		position.y = player.getPosition().y + verticDistance;
+	}
+	
+	private float calculateHorizontalDistance(){
+		return (float) (distanceFromPlayer * Math.cos(Math.toRadians(pitch)));
+	}
+	
+	private float calculateVerticalDistance(){
+		return (float) (distanceFromPlayer * Math.sin(Math.toRadians(pitch)));
+	}
+	
+	private void calculateZoom(){
+		
+		float zoomLevel = Mouse.getDWheel() * 0.1f;
+		if(distanceFromPlayer-zoomLevel<=distanceMaxPlayer && distanceFromPlayer-zoomLevel>=distanceMinPlayer){
+		distanceFromPlayer -= zoomLevel;}
+		else if (distanceFromPlayer-zoomLevel>distanceMaxPlayer){
+			distanceFromPlayer = distanceMaxPlayer;
+		}
+		else {
+			distanceFromPlayer = distanceMinPlayer;
+		}
+		
+	}
+	
+	private void calculatePitch(){
+		if(Mouse.isButtonDown(1)){
+			float pitchChange = Mouse.getDY() * 0.1f;
+			if(pitch-pitchChange<=pitchMax && pitch-pitchChange>=pitchMin){
+			pitch -= pitchChange;}
+			else if(pitch-pitchChange>pitchMax){
+				pitch = pitchMax;
+			}
+			else {
+				pitch = pitchMin;
+			}
+		}
+	}
+	
+	private void calculateAngleAroundPlayer(){
+		if(Mouse.isButtonDown(1)){
+			float angleChange = Mouse.getDX() * 0.3f;
+			angleAroundPLayer -= angleChange;
+		}
 	}
 	
 	
